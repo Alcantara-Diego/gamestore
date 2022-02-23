ab5b887de0068564133d8453e7894c87957a1ced
Diego Alcântara <ddag0309@gmail.com>
2022-02-23 12:14:33 -0300

    Seção de destaques adicionada

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Highlights from './Components/Highlights';
 function App() {
   return (
     <div className="App">
+
+      <div className="alert fw-bold">
+        <p className="m-0 d-inline mx-1">Item adicionado ao carrinho</p>  
+        <i className="bi bi-cart-fill"></i>
+        <i className="bi bi-check-circle-fill"></i>
+      </div>
       <Nav/>
       <HeroSection/>
       <Highlights/>
--- src/Components/Highlights.js
+++ src/Components/Highlights.js
@@ -1,8 +1,81 @@
 function Highlights(){
+
+    function itemAddedToCart(){
+        let cartAlert = document.getElementsByClassName("alert")[0]
+        
+        cartAlert.style.animation="showAlert 4s";
+
+        setTimeout( ()=>{
+            cartAlert.style.animation="none";
+        }, 4000);
+    }
     return(
         <section className="highlightsSection w-100">
-            <div>
-                <img src="../assets/highlightImages/gowHighlight.jpg" alt="."></img>
+
+            <div id="hilight1" className="highlight">
+
+                <div className="highlightActionButtons">
+                    <h1 className="itemTitle">Horizon</h1>
+                    <button className="btn actionButton">Comprar!</button>
+                    <button className="btn actionButton" onClick={itemAddedToCart}>Adicionar ao carrinho
+                    <i className="bi bi-cart-fill"></i>
+                    </button>
+                </div>
+
+                <div className="highlightBottomInfo">
+                    <div className="highlightName">Destaque</div>
+                    <div className="price">R$249,99</div>
+                </div>
+
+            </div>
+            <div id="hilight2" className="highlight">
+
+
+                <div className="highlightActionButtons">
+                <h1 className="itemTitle">God of War</h1>
+                    <button className="btn actionButton">Comprar!</button>
+                    <button className="btn actionButton" onClick={itemAddedToCart}>Adicionar ao carrinho
+                    <i className="bi bi-cart-fill"></i>
+                    </button>
+                </div>
+
+                <div className="highlightBottomInfo">
+                    <div className="highlightName">Novo</div>
+                    <div className="price">R$299,99</div>
+                </div>
+
+            </div>
+            <div id="hilight3" className="highlight">
+
+                <div className="highlightActionButtons">
+                <h1 className="itemTitle">Resident Evil</h1>
+                    <button className="btn actionButton">Comprar!</button>
+                    <button className="btn actionButton" onClick={itemAddedToCart}>Adicionar ao carrinho
+                    <i className="bi bi-cart-fill"></i>
+                    </button>
+                </div>
+
+                <div className="highlightBottomInfo">
+                    <div className="highlightName">Oferta</div>
+                    <div className="price">R$99,99</div>
+                </div>
+
+            </div>
+            <div id="hilight4" className="highlight">
+
+                <div className="highlightActionButtons">
+                <h1 className="itemTitle">Rainbow Six</h1>
+                    <button className="btn actionButton">Comprar!</button>
+                    <button className="btn actionButton" onClick={itemAddedToCart}>Adicionar ao carrinho
+                    <i className="bi bi-cart-fill"></i>
+                    </button>
+                </div>
+
+                <div className="highlightBottomInfo">
+                    <div className="highlightName">Oferta</div>
+                    <div className="price">R$99,99</div>
+                </div>
+
             </div>
 
         </section>
