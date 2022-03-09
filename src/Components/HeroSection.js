eee2c7d6bcb5689e6738d9e0c67745cb69374c42
Diego Alcântara <ddag0309@gmail.com>
2022-03-09 15:16:32 -0300

    hot fix
    Corrigindo bug em que a aba "Mais detalhes" não é encontrada

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection(){
                 <div className="buyItemHeroSection w-100 fw-bold d-flex flex-column justify-content-center align-items-center">
                     <p>Spider-Man Miles Morales PS5</p>
 
-                    <a href='gamestore/#/detalhes' className="actionButton px-4 py-2">Comprar agora!</a>
+                    <a href='/#/detalhes' className="actionButton px-4 py-2">Comprar agora!</a>
                 </div>
 
                 <div className="navigateBetweenSectionCards mt-5 d-flex flex-row justify-content-center">
